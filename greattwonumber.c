a4a2d8aab5bc0747a361a3b0d33292e6ff5ba224
tinusam27 <47379204+tinusam27@users.noreply.github.com>
2019-02-13 12:35:58 +0530

    Add files via upload

---
+++ greattwonumber.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	printf("enter the value of b");
+	scanf("%d",&b);
+	if (a>b)
+		{
+			printf("the greatest number is %d",a);
+		}
+	else
+		{
+			printf("the greatest number is %d",b);
+		}
+}
+	
