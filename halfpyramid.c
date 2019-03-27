318d6a7b284aefe0be320a4a72a36322e47ea74e
tinusam27 <47379204+tinusam27@users.noreply.github.com>
2019-03-27 11:48:55 +0530

    Add files via upload

---
+++ halfpyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,j,range;
+printf("enter the range");
+scanf("%d",&range);
+for(i=o;i<=range;i++)
+	{
+		for(j=1;j<=i,j++)
+		printf("%d",j);
+	printf("\n");
+	}
+}
