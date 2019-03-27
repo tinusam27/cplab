7afcb6f8e116cd2749bf6a76b7af0a70523b7b88
tinusam27 <47379204+tinusam27@users.noreply.github.com>
2019-03-27 11:49:15 +0530

    Add files via upload

---
+++ hour.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+voidmain()
+{
+	int time,i,j;
+	printf("12 midnight\n");
+	for(i=1;i<12;i++)
+		printf("%dAM\n",i);
+			printf("12noon\n");
+			for(j=1,j<=11,j++)
+			printf("%dPM\n",j);
+}
